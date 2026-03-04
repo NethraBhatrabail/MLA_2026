3953a9650b955bd43ea585d0416e27be7e02a913
Nethra K1 <nethra.k1@mphasis.com>
2026-03-04 12:06:45 +0530

    Cloud

---
+++ SBCloudAPIGatewayP2/src/main/java/com/test/controller/Product2Controller.java
@@ -0,0 +1,17 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product2")
+public class Product2Controller {
+	
+	@GetMapping("/service2")
+	public String productService2()
+	{
+		return "This is a simple Spring Cloud API Service-2";
+	}
+
+}
